113351a22a163afb30a2ef9a6dbf3c113e2ab2cc
k022g0004@g.neec.ac.jp <k022g0004@g.neec.ac.jp>
2023-12-21 12:11:24 +0900

    あ

--- main.cpp
+++ main.cpp
@@ -1,12 +1,7 @@
 #include<Windows.h>
-#include"Framework.h"
-#include"MyGame.h"
 
 //Windowsアプリでのエントリーポイント(main関数)
 int WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
-	Framework* game = new MyGame;
-	game->Run();
-	delete game;
 	return 0;
 }
