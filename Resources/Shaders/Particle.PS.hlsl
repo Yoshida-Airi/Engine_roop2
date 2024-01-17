3f72f69ea4d54613bddbea63bd9b3281bf1ff881
k022g0004@g.neec.ac.jp <k022g0004@g.neec.ac.jp>
2024-01-17 15:24:24 +0900

    コマンドリストをシェーダーのものに変えればつかえるかもしれない。使えないかもしれない。

---
+++ Resources/Shaders/Particle.PS.hlsl
@@ -0,0 +1,66 @@
+#include"Particle.hlsli"
+
+struct Material
+{
+    float32_t4 color;
+    float32_t4x4 uvTransform;
+    int32_t enableLighting;
+};
+
+struct DirectionalLight
+{
+    float32_t4 color;
+    float32_t3 direction;
+    float intensity;
+};
+
+
+ConstantBuffer<Material> gMaterial : register(b0);
+ConstantBuffer<DirectionalLight> gDirectionalLight : register(b1);
+Texture2D<float32_t4> gTexture : register(t0);
+SamplerState gSampler : register(s0);
+
+struct PixcelShaderOutput
+{
+    float32_t4 color : SV_TARGET0;
+};
+
+PixcelShaderOutput main(VertexShaderOutput input)
+{
+    PixcelShaderOutput output;
+    float4 transformedUV = mul(float32_t4(input.texcoord, 0.0f, 1.0f), gMaterial.uvTransform);
+    float32_t4 textureColor = gTexture.Sample(gSampler, transformedUV.xy);
+    
+    if (textureColor.a <= 0.5)
+    {
+        discard;
+    }
+    if (textureColor.a == 0.5)
+    {
+        discard;
+    }
+
+    
+    
+    if (gMaterial.enableLighting != 0)
+    {
+        float NdotL = saturate(dot(normalize(input.normal), -gDirectionalLight.direction));
+        float cos = pow(NdotL * 0.5f + 0.5f, 2.0f);
+        output.color.rgb = gMaterial.color.rgb * textureColor.rgb * gDirectionalLight.color.rgb * cos * gDirectionalLight.intensity;
+        if (output.color.a == 0.0)
+        {
+            discard;
+        }
+        output.color.a = gMaterial.color.a * textureColor.a;
+    }
+    else
+    {
+        output.color.rgb = gMaterial.color.rgb * textureColor.rgb;
+        if (output.color.a == 0.0)
+        {
+            discard;
+        }
+        output.color.a = gMaterial.color.a * textureColor.a;
+    }
+    return output;
+}
---
+++ Resources/Shaders/Particle.VS.hlsl
@@ -0,0 +1,38 @@
+#include"Particle.hlsli"
+
+struct TransformationMatrix
+{
+    float32_t4x4 WorldMatrix;
+};
+
+struct ViewProjectionMatrix
+{
+    float32_t4x4 view;
+    float32_t4x4 projection;
+};
+
+ConstantBuffer<TransformationMatrix> gTransformationMatrix : register(b0);
+ConstantBuffer<ViewProjectionMatrix> gViewProjectionMatrix : register(b1);
+
+
+struct VertexShaderInput
+{
+    float32_t4 position : POSITION0;
+    float32_t2 texcoord : TEXCOORD0;
+    float32_t3 normal : NORMAL0;
+};
+
+VertexShaderOutput main(VertexShaderInput input)
+{
+    VertexShaderOutput output;
+    
+    output.texcoord = input.texcoord;
+
+    // 通常カメラ
+    float32_t4x4 ViewProjectionMatrix = mul(gViewProjectionMatrix.view, gViewProjectionMatrix.projection);
+    float32_t4x4 WorldViewProjectionMatrix = mul(gTransformationMatrix.WorldMatrix, ViewProjectionMatrix);
+    output.position = mul(input.position, WorldViewProjectionMatrix);
+    output.normal = normalize(mul(input.normal, (float32_t3x3) gTransformationMatrix.WorldMatrix));
+    
+    return output;
+}
