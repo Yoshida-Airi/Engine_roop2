c2f450b9dc2d1550d0c6d89433d68351c82b5034
k022g0004@g.neec.ac.jp <k022g0004@g.neec.ac.jp>
2024-01-29 15:28:45 +0900

    タイトルに動きをつける

--- Application/Scene/TitleScene.cpp
+++ Application/Scene/TitleScene.cpp
@@ -4,7 +4,7 @@
 TitleScene::~TitleScene()
 {
 	delete camera;
-
+	delete uiCamera;
 }
 
 void TitleScene::Initialize()
@@ -15,10 +15,24 @@ void TitleScene::Initialize()
 	camera = new Camera;
 	camera->Initialize();
 
+	uiCamera = new UICamera;
+	uiCamera->Initialize();
+
+	titleTex = TextureManager::GetInstance()->LoadTexture("Resources/title.png");
+	ABottonTex = TextureManager::GetInstance()->LoadTexture("Resources/ABotton.png");
 
 	fence_ = Model::Create("Resources", "fence.obj");
-	cube_ = Model::Create("Resources", "cube.obj");
+	fence_->SetisInvisible(true);
 
+	cube_ = Model::Create("Resources", "enemy.obj");
+
+	
+
+	titleSprite.reset(Sprite::Create(titleTex));
+	ABottonSprite.reset(Sprite::Create(ABottonTex));
+	ABottonSprite->SetPosition({ 620.0f,500.0f });
+
+	InitializeFloatingGimmick();
 	
 }
 
@@ -27,8 +41,12 @@ void TitleScene::Update()
 	camera->CameraDebug();
 	camera->UpdateMatrix();
 
+	uiCamera->UpdateMatrix();
+
 	input->TriggerKey(DIK_0);
 
+
+	//シーン遷移
 	if (input->TriggerKey(DIK_RETURN))
 	{
 		sceneManager_->ChangeScene("GAMEPLAY");
@@ -46,11 +64,15 @@ void TitleScene::Update()
 		
 	}
 
+	UpAndDownMotion(1.5f);
+
+
 	fence_->Update();
 	cube_->Update();
 
+	titleSprite->Update();
+	ABottonSprite->Update();
 
-	
 	cube_->ModelDebug("cube");
 	fence_->ModelDebug("fence");
 
@@ -58,6 +80,25 @@ void TitleScene::Update()
 		1.0f,1.0f,1.0f,1.0f,
 		3.0f,0.0f,1.0f,2.0f,
 	};
+
+	bool drawEnabled = true;
+	flashTime++;
+
+	// 描画を点滅させるための処理
+	if (drawEnabled && flashTime >= 40) {
+		ABottonSprite->SetisInvisible(false);
+		
+	}
+
+	if (flashTime >= 80)
+	{
+
+		// フラグをトグル
+		ABottonSprite->SetisInvisible(true);
+		drawEnabled = !drawEnabled;
+		flashTime = 0;
+	}
+
 	
 }
 
@@ -65,5 +106,33 @@ void TitleScene::Draw()
 {
 	fence_->Draw(camera);
 	cube_->Draw(camera);
+
+
+	titleSprite->Draw(uiCamera);
+	ABottonSprite->Draw(uiCamera);
+}
+
+
+void TitleScene::InitializeFloatingGimmick() {
+
+	//浮遊ギミックの媒介変数
+	UpdownParameter_ = 0.0f;
+	swingParameter_ = 0.0f;
+	//浮遊移動のサイクル<frame>
+	uint16_t cycle_ = 60;
+	//浮遊の振動<m>
+	amplitude_ = 1.0f;
+}
+
+void TitleScene::UpAndDownMotion(float time)
+{
+	//1フレームでのパラメータ加算値
+	const float step = time * 3.14f / cycle_;
+	//パラメータを１ステップ分加算
+	UpdownParameter_ += step;
+	//2πを超えたら０に戻す
+	UpdownParameter_ = std::fmod(UpdownParameter_, 2.0f * 3.14f);
+	//浮遊を座標に反映
+	cube_->worldTransform_->translation_.y = (std::sin(UpdownParameter_) * amplitude_);
 }
 
--- Application/Scene/TitleScene.h
+++ Application/Scene/TitleScene.h
@@ -24,17 +24,40 @@ public:
 	void Update()override;
 	void Draw()override;
 
+	void InitializeFloatingGimmick();
+
+	void UpAndDownMotion(float time);
+
 private:
 
 	Input* input;
 	SceneManager* sceneManager_ = nullptr;
 	ICamera* camera;
+	ICamera* uiCamera;
 
 
-
+	uint32_t titleTex;
+	std::unique_ptr<Sprite>titleSprite = nullptr;
+	uint32_t ABottonTex;
+	std::unique_ptr<Sprite>ABottonSprite = nullptr;
 
 	std::unique_ptr<Model> fence_ = nullptr;
 	std::unique_ptr<Model> cube_ = nullptr;
+
+
+	//浮遊ギミックの媒介変数
+	float UpdownParameter_ = 0.0f;
+	float swingParameter_ = 0.0f;
+	float throwParameter_ = 0.0f;
+	//浮遊移動のサイクル<frame>
+	uint16_t cycle_ = 60;
+	//浮遊の振動<m>
+	float amplitude_ = 0.1f;
+	float swing_ = 0.1f;
+	float throw_ = 0.1f;
+	bool isMove = false;
+
+	float flashTime = 0.0f;
 	
 };
 
