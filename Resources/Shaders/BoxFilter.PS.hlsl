b588c041886463e31dda6d587be2eb03c00505b9
k022g0004@g.neec.ac.jp <k022g0004@g.neec.ac.jp>
2024-06-05 15:36:26 +0900

    3と5️の切り替えできないけどたぶんできてる？

--- Resources/Shaders/BoxFilter.PS.hlsl
+++ Resources/Shaders/BoxFilter.PS.hlsl
@@ -10,6 +10,15 @@ static const float32_t2 kIndex3x3[3][3] =
     { { -1.0f, 1.0f }, { 0.0f, 1.0f }, { 1.0f, 1.0f } },
 };
 
+static const float32_t2 kIndex5x5[5][5] =
+{
+    { { -1.0f, -1.0f }, { -1.0f, -1.0f }, { 0.0f, -1.0f }, { 1.0f, -1.0f }, { 1.0f, -1.0f } },
+    { { -1.0f, -1.0f }, { -1.0f, -1.0f }, { 0.0f, -1.0f }, { 1.0f, -1.0f }, { 1.0f, -1.0f } },
+    { { -1.0f, 0.0f }, { -1.0f, 0.0f }, { 0.0f, 0.0f }, { 1.0f, 0.0f }, { 1.0f, 0.0f } },
+    { { -1.0f, 1.0f }, { -1.0f, 1.0f }, { 0.0f, 1.0f }, { 1.0f, 1.0f }, { 1.0f, 1.0f } },
+    { { -1.0f, 1.0f }, { -1.0f, 1.0f }, { 0.0f, 1.0f }, { 1.0f, 1.0f }, { 1.0f, 1.0f } },
+};
+
 static const float32_t kKernel3x3[3][3] =
 {
     { 1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f },
@@ -42,13 +51,14 @@ PixelShaderOutput main(VertexShaderOutput input)
     output.color.rgb = float32_t3(0.0f, 0.0f, 0.0f);
     output.color.a = 1.0f;
     
+    
     //2.3x3ループ
     for (int32_t x = 0; x < 5;++x)
     {
         for (int32_t y = 0; y < 5; ++y)
         {
             //3.現在のtexcoordを算出
-            float32_t2 texcoord = input.texcoord + kIndex3x3[x][y] * uvStepSize;
+            float32_t2 texcoord = input.texcoord + kIndex5x5[x][y] * uvStepSize;
             //4.色に1/9掛けて足す
             float32_t3 fetchColor = gTexture.Sample(gSampler, texcoord).rgb;
             output.color.rgb += fetchColor * kKernel5x5[x][y];
